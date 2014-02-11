99bdcb4f85e4f36c5b5702d4cbbf710441ca00ab
Laura Crowe <lleecrowe@gmail.com>
2014-02-11 16:05:15 -0600

    add new user to db once authenticated by twitter

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -13,13 +13,18 @@ get '/sign_out' do
 end
 
 get '/auth' do
+
   # the `request_token` method is defined in `app/helpers/oauth.rb`
   @access_token = request_token.get_access_token(:oauth_verifier => params[:oauth_verifier])
+
   # our request token is only valid until we use it to get an access token, so let's delete it from our session
   session.delete(:request_token)
 
   # at this point in the code is where you'll need to create your user account and store the access token
 
+  @user = User.find_or_create_by(username: @access_token.params[:screen_name])
+  @user.update(oauth_token: @access_token.token, oauth_secret: @access_token.secret)
+  @user.save
+
   erb :index
-  
 end
